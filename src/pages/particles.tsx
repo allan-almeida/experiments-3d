b3de91e821491ec7169f0e07dc2ac12688cd033a
allan almeida <allan.almeida91@gmail.com>
2020-02-18 01:17:52 +1100

    rendered basic particles

--- src/pages/particles.tsx
+++ src/pages/particles.tsx
@@ -13,6 +13,19 @@ export default () => {
       <Canvas>
         <Controls type='trackball' />
         <Particles />
+        <group>
+          <pointLight intensity={0.3} />
+          <ambientLight intensity={1} />
+          <spotLight
+            castShadow
+            intensity={0.2}
+            angle={Math.PI / 7}
+            position={[150, 150, 250]}
+            penumbra={1}
+            shadow-mapSize-width={2048}
+            shadow-mapSize-height={2048}
+          />
+        </group>
       </Canvas>
     </IndexLayout>
   )

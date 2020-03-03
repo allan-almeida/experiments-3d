ba5a2a276eaf5d8d51b26754894fdd28ef5132b1
allan almeida <allan.almeida91@gmail.com>
2020-03-03 12:27:21 +1100

    configured spring plane

--- src/components/particles/Swarm.tsx
+++ src/components/particles/Swarm.tsx
@@ -7,16 +7,37 @@ const count = 512
 
 const colors = ['#A2CCB6', '#FCEEB5', '#EE786E', '#e0feff', 'lightpink', 'lightblue']
 
-const randomize = i => {
+const randomize = (i: number) => {
   return {
-    position: [100 - Math.random() * 200, 100 - Math.random() * 200, i * 1.5]
+    position: [
+      Math.random() * 4000 - 2000,
+      Math.random() * 4000 - 2000,
+      Math.random() * 4000 - 2000
+    ]
+  }
+}
+
+const plane = (i: number) => {
+  console.log('plane')
+  // Plane
+  const amountX = 16
+  const amountZ = 32
+  const separation = 150
+  const offsetX = ((amountX - 1) * separation) / 2
+  const offsetZ = ((amountZ - 1) * separation) / 2
+
+  const x = (i % amountX) * separation
+  const z = Math.floor(i / amountX) * separation
+  const y = (Math.sin(x * 0.5) + Math.sin(z * 0.5)) * 200
+
+  return {
+    position: [x - offsetX, y, z - offsetZ]
   }
 }
 
 const data = new Array(count).fill().map(() => {
   return {
-    color: colors[Math.round(Math.random() * (colors.length - 1))],
-    position: [random(-9, 9, true), random(-9, 9, true), random(-9, 9, true)]
+    color: colors[Math.round(Math.random() * (colors.length - 1))]
   }
 })
 
@@ -41,17 +62,16 @@ export const Swarm: React.FC = () => {
   // Refs
   const meshRef = useRef()
 
-  const [springs, set] = useSprings(count, i => ({
-    from: randomize(i),
+  const [springs, set] = useSprings(count, (i: number) => ({
     ...randomize(i),
-    config: { mass: 20, tension: 150, friction: 50 }
+    config: { mass: 5, tension: 150, friction: 50 }
   }))
 
-  useEffect(() => void setInterval(() => set(i => ({ ...randomize(i) })), 3000), [set])
+  useEffect(() => void setInterval(() => set(i => ({ ...plane(i) })), 3000), [])
 
   return data.map((d, i) => (
     <a.mesh key={i} {...springs[i]}>
-      <sphereBufferGeometry attach="geometry" args={[5, 32, 32]} />
+      <sphereBufferGeometry attach="geometry" args={[20, 32, 32]} />
       <meshStandardMaterial attach="material" color={d.color} roughness={0.75} metalness={0.5} />
     </a.mesh>
   ))
